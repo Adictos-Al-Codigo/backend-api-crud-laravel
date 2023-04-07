4b86465d19abe9768fd7de2f88001de4fbc53452
Celorio2001 <jahir.celorio2001@gmail.com>
2023-04-07 00:04:25 -0500

    Categoria Crud

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -17,27 +17,34 @@ class CategoriaController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $valiData = $request->validate([
+            'nombre' => 'required|string|max:255'
+        ]);
+
+        $categoria = Categoria::create([
+            'nombre' => $valiData['nombre'],
+            'estado' => 1
+        ]);
+
+        return response()->json(['message'=>'Categoria Registrada Correctamente.',200]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Categoria $categoria)
+    public function show($id)
     {
-        //
+        $categoria = Categoria::find($id);
+
+        if (is_null($categoria)) {
+            return response()->json(['message'=>'Categoria no encontrada',404]);
+        }
+
+        return response()->json([$categoria,'message'=>'Categoria Encontrada',200]);
     }
 
     /**
@@ -51,16 +58,39 @@ class CategoriaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Categoria $categoria)
+    public function update(Request $request,$id)
     {
-        //
+
+        $categoria = Categoria::find($id);
+
+        if (is_null($categoria)) {
+            return response()->json(['message' => 'Categoria no Encontrada',404]);
+        }
+
+        $validData = $request->validate([
+            'nombre' => 'required|string|max:255'
+        ]);
+
+        $categoria->nombre = $validData['nombre'];
+        $categoria->estado = 1;
+
+        return response()->json([$categoria,'message' => 'Categoria Actualizada']);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Categoria $categoria)
+    public function destroy($id)
     {
-        //
+        $categoria = Categoria::find($id);
+
+        if (is_null($categoria)) {
+            return response()->json(['message' => 'Categoria No Encontrada',404]);
+        }
+
+        $categoria->estado = 0;
+        $categoria->save();
+
+        return response()->json(['message' => 'Categoria Eliminada',200]);
     }
 }
